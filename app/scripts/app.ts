13943bbd6b8f066c522c8636de4781bda2ad3e40
Kyle <kyle.rits.rodgers@gmail.com>
2017-05-23 13:55:21 -0400

    Added jQuery and integrated it with Typescript

--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -1,4 +1,9 @@
+// Imports
+
 import { sayHello } from "./greeter";
+import * as $ from 'jquery';
+
+// Functions
 
 function showHello(divName: string, name: string) {
   const elt = document.getElementById(divName);
